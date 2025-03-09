28daefdf151ebaba0acae624cdfa03aa805bffb7
Toprak <toprak1771@gmail.com>
2025-03-10 02:36:07 +0300

    add cors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 const express = require("express");
+const cors = require("cors");
 const PuppeteerRoute = require("../src/router/puppeteer.route");
 const app = express();
 
 const port = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  cors({
+    credentials:true,
+    origin: ["*","http://localhost:8000/","http://127.0.0.1:8000"],
+  })
+);
+
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
